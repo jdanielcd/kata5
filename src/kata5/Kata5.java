044fe916f776c2df4f5c6007e91aa19fe70343b7
usuario <usuario@10.22.146.201>
2013-11-22 14:01:32 +0000

    Version 03 - Histograma genérico completo

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -5,11 +5,11 @@ public class Kata5 {
     public static void main(String[] args) {
         PersonLoader loader = new MockPersonLoader();
         HistogramBuilder<Person> builder = new HistogramBuilder<>(loader.load());
-        HistogramViewer<Person> viewer = new HistogramViewer<>();
+        HistogramViewer<String> viewer = new HistogramViewer<>();
         viewer.show(builder.build(createAttributeExtractor()));
     }
     
-    public AttributeExtractor<Person, String> createAttributeExtractor(){
+    public static AttributeExtractor<Person, String> createAttributeExtractor(){
         return new AttributeExtractor<Person, String>() {
             @Override
             public String extract(Person person) {
